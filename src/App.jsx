72ce8f3b31ddf42ab2b6fb22a8b978cea30fbb14
Yaskrazz <macbook@Guramee.local>
2023-12-04 12:30:29 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-600 ">
+      Techcom Fest 2023
+    </h1>
+        
+    </>
+  )
+}
+
+export default App
